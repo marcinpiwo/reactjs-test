b344afb65cf6bb3b263b4573caec642cff570dba
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-05-27 19:05:09 +0100

    bootstrap menu implemented

--- frontend/alltypes.d.ts
+++ frontend/alltypes.d.ts
@@ -1,3 +1,4 @@
+declare module 'react-bootstrap';
 declare module 'react-materialize';
 declare module 'react-router';
 declare module 'react-router-dom';
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import './App.css';
+import Menu from './components/navmenu/Menu';
 
 class App extends React.Component {
   public render() {
     return (
       <BrowserRouter>
+    
         <div className="App">
+        <Menu />
           <Route path='/' exact strict component={Home} />
         </div>
       </BrowserRouter>
