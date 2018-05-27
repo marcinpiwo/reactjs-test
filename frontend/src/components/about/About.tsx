5a916bf67e0dcf945330ee7068cbedd8f311ddf8
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-05-27 17:42:49 +0100

    added about and buy placement match template screens

---
+++ frontend/src/components/about/About.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+class About extends React.Component {
+    render() {
+      return (     
+        <div className="About">
+         
+            <h1>About Page</h1>
+           
+        </div>    
+      );
+    }
+  }
+  
+  export default About;
+  
---
+++ frontend/src/components/buyBoost/buyPlacementMatch/buyPlacementMatch.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+class BuyPlacementMatch extends React.Component {
+    render() {
+      return (     
+        <div className="buyPlacementMatch">
+         
+            <h1>BuyPlacementMatch Page</h1>
+           
+        </div>    
+      );
+    }
+  }
+  
+  export default BuyPlacementMatch;
+  
