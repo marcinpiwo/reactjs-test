430df8a0a5a744b6640a5cff104f9c24fe04ad35
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-05-27 22:15:15 +0100

    bootstrap files added to public file, tabs added to buy boost

--- frontend/src/components/buyBoost/BuyBoost.tsx
+++ frontend/src/components/buyBoost/BuyBoost.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
+import { Tabs, Tab } from 'react-bootstrap';
+
 
 class BuyBoost extends React.Component {
-    render() {
-      return (     
-        <div className="BuyBoost">
+  render() {
+    return (
+      <div className="container">
+        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+          <Tab eventKey={1} title="Placement Match">
+            Placement Match
+          </Tab>
+          <Tab eventKey={2} title="Wins Boost">
+            Wins Boost
+          </Tab>
+        </Tabs>
 
-            <h1>BuyBoost Page</h1>
-           
-        </div>    
-      );
-    }
+      </div>
+    );
   }
-  
-  export default BuyBoost;
+}
+
+export default BuyBoost;
+
